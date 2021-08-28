ebc9e7066d06b963dd90e8ad78b961da93b162fb
tommycp96 <tommycp8@gmail.com>
2021-08-28 20:02:04 +0700

    208. What's the Context Doing?

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import { FlatList } from 'react-native-gesture-handler'
 import createDataContext from './createDataContext'
 
 const authReducer = (state, action) => {
@@ -7,8 +8,32 @@ const authReducer = (state, action) => {
   }
 }
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+    // if we sign up, modify our state, and say that we are authenticated
+    /* if signing up fails, we probably need to reflect an error message
+       somehow
+    */
+  }
+}
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message (somehow)
+  }
+}
+
+const signout = (dispatch) => {
+  return () => {
+    // somehow sign out!!!
+  }
+}
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signup, signout },
   { isSignedIn: false }
 )
