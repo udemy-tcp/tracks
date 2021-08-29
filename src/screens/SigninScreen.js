1d125e3fc1274ea5a040ae4846c9124b3be094c5
tommycp96 <tommycp8@gmail.com>
2021-08-29 20:04:56 +0700

    228. Signing Out a User

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -3,10 +3,10 @@ import { View, StyleSheet, Text } from 'react-native'
 import { NavigationEvents } from 'react-navigation'
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink'
-import { Context } from '../context/AuthContext'
+import { Context as AuthContext } from '../context/AuthContext'
 
 const SigninScreen = () => {
-  const { state, signin, clearErrorMessage } = useContext(Context)
+  const { state, signin, clearErrorMessage } = useContext(AuthContext)
 
   return (
     <View style={styles.container}>
