05c7d5536c86595cf5ec5ebecd11ce159603dc75
tommycp96 <tommycp8@gmail.com>
2021-08-29 09:53:25 +0700

    215. Navigation From Outside of React

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -26,6 +27,8 @@ const signup =
       const response = await trackerApi.post('/signup', { email, password })
       await AsyncStorage.setItem('token', response.data.token)
       dispatch({ type: 'signup', payload: response.data.token })
+
+      navigate('TrackList')
     } catch (err) {
       dispatch({
         type: 'add_error',
