b1f0d0d2b53b170001eec5cd84841c2c4c98f6c3
tommycp96 <tommycp8@gmail.com>
2021-09-18 16:36:11 +0700

    250. Automatic Updates

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -5,13 +5,17 @@ import {
   watchPositionAsync
 } from 'expo-location'
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null)
+  const [subscriber, setSubscriber] = useState(null)
 
   const startWatching = async () => {
     try {
       const { granted } = await requestForegroundPermissionsAsync()
-      const subscriber = await watchPositionAsync(
+      if (!granted) {
+        throw new Error('Location permission not granted')
+      }
+      const sub = await watchPositionAsync(
         {
           accuracy: Accuracy.BestForNavigation,
           timeInterval: 1000,
@@ -19,17 +23,20 @@ export default (callback) => {
         },
         callback
       )
-      if (!granted) {
-        throw new Error('Location permission not granted')
-      }
+      setSubscriber(sub)
     } catch (e) {
       setErr(e)
     }
   }
 
   useEffect(() => {
-    startWatching()
-  }, [])
+    if (shouldTrack) {
+      startWatching()
+    } else {
+      subscriber.remove()
+      setSubscriber(null)
+    }
+  }, [shouldTrack])
 
   return [err]
 }
