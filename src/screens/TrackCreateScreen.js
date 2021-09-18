09ee8682b5dde69b6d81d8bb3e5eee8728d33dee
tommycp96 <tommycp8@gmail.com>
2021-09-18 15:13:24 +0700

    245. Live Location Updates

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 import '../_mockLocation'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
@@ -9,8 +9,10 @@ import {
   Accuracy
 } from 'expo-location'
 import Map from '../components/Map'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext)
   const [err, setErr] = useState(null)
 
   const startWatching = async () => {
@@ -22,7 +24,7 @@ const TrackCreateScreen = () => {
           timeInterval: 1000,
           distanceInterval: 10
         },
-        (location) => console.log(location)
+        (location) => addLocation(location)
       )
       if (!granted) {
         throw new Error('Location permission not granted')
