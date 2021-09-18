ea3351beb8168b2010b8b6cfe83a5f89b604cad9
tommycp96 <tommycp8@gmail.com>
2021-09-18 16:07:36 +0700

    248. Extracting Location Tracking(to resuable custom hook)

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,42 +1,16 @@
 import '../_mockLocation'
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy
-} from 'expo-location'
 import Map from '../components/Map'
 import { Context as LocationContext } from '../context/LocationContext'
+import useLocation from '../hooks/useLocation'
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext)
-  const [err, setErr] = useState(null)
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync()
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10
-        },
-        (location) => addLocation(location)
-      )
-      if (!granted) {
-        throw new Error('Location permission not granted')
-      }
-    } catch (e) {
-      setErr(e)
-    }
-  }
-
-  useEffect(() => {
-    startWatching()
-  }, [])
+  const [err] = useLocation(addLocation)
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
