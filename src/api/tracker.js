68c0ae01910bb587ed60fd0181695dc039bf0829
tommycp96 <tommycp8@gmail.com>
2021-11-28 11:47:21 +0700

    266. Automatic Authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
-import axios from 'axios'
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://tcp-track-server.herokuapp.com'
-})
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
