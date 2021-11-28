15c417af6213dfa643758788dbdc2e359ddea723
tommycp96 <tommycp8@gmail.com>
2021-11-28 08:48:55 +0700

    260. Avoiding Stale References

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,48 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import {
   Accuracy,
   requestForegroundPermissionsAsync,
   watchPositionAsync
-} from 'expo-location'
+} from 'expo-location';
 
 export default (shouldTrack, callback) => {
-  const [err, setErr] = useState(null)
-  const [subscriber, setSubscriber] = useState(null)
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync()
-      if (!granted) {
-        throw new Error('Location permission not granted')
-      }
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10
-        },
-        callback
-      )
-      setSubscriber(sub)
-    } catch (e) {
-      setErr(e)
-    }
-  }
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestForegroundPermissionsAsync();
+        if (!granted) {
+          throw new Error('Location permission not granted');
+        }
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          },
+          callback
+        );
+      } catch (e) {
+        setErr(e);
+      }
+    };
+
     if (shouldTrack) {
-      startWatching()
+      startWatching();
     } else {
-      subscriber.remove()
-      setSubscriber(null)
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
 
     return () => {
       if (subscriber) {
-        subscriber.remove()
+        subscriber.remove();
       }
-    }
-  }, [shouldTrack, callback])
+    };
+  }, [shouldTrack, callback]);
 
-  return [err]
-}
+  return [err];
+};
