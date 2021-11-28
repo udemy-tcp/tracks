91183f1dcc8b866afaafbe6486817490c622f217
tommycp96 <tommycp8@gmail.com>
2021-11-28 09:00:02 +0700

    262. Bring Back the Polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,20 @@
-import React, { useContext } from 'react'
-import { Text, StyleSheet, ActivityIndicator } from 'react-native'
-import MapView, { Polyline, Circle } from 'react-native-maps'
-import { Context as LocationContext } from '../context/LocationContext'
+import React, { useContext } from 'react';
+import { Text, StyleSheet, ActivityIndicator } from 'react-native';
+import MapView, { Polyline, Circle } from 'react-native-maps';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
   const {
-    state: { currentLocation }
-  } = useContext(LocationContext)
+    state: { currentLocation, locations }
+  } = useContext(LocationContext);
 
   if (!currentLocation)
-    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
 
   const initialLocation = {
     longitude: -122.0312186,
     latitude: 37.33233141
-  }
+  };
 
   return (
     <MapView
@@ -31,14 +31,15 @@ const Map = () => {
         strokeColor="rgba(158,158,255, 1.0)"
         fillColor="rgba(158,158,255, 0.3)"
       />
+      <Polyline coordinates={locations.map((loc) => loc.coords)} />
     </MapView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   map: {
     height: 300
   }
-})
+});
 
-export default Map
+export default Map;
